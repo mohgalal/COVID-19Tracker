c383285d3104e472bfd5e667dc40a9d624ab8cdf
Mohamed Soliman <mohamedelzmar2018@gmail.com>
2021-06-25 18:52:59 +0200

    starting for configuration language

--- app/src/main/java/com/example/covid_19tracker/fragments/AdviceFragment.java
+++ app/src/main/java/com/example/covid_19tracker/fragments/AdviceFragment.java
@@ -44,27 +44,22 @@ public View onCreateView(LayoutInflater inflater, ViewGroup container,
         listAdvice = view.findViewById(R.id.advice_fragment_lv);
         models = new ArrayList<>();
 
-        models.add(new AdviceModel(R.drawable.ic_covid_protection_mask_healthcare_contagion_icon,"You must wear a full mask as a precaution in hospitals and closed crowded places "));
-        models.add(new AdviceModel(R.drawable.ic_water_wash_hand_washing_healthcare_icon,"Wash hands thoroughly with soap and water or disinfectants, especially after coughing or sneezing or using toilets"));
-        models.add(new AdviceModel(R.drawable.ic_avoid_public_places_icon,"Avoid contact with people with respiratory symptoms and maintain a distance of at least 1 meter between yourself and anyone with symptoms"));
-        models.add(new AdviceModel(R.drawable.ic_raw_meat_no_avoid_eat_icon,"Do not eat any animal products uncooked, and guard not to mix them with other flavors"));
-        models.add(new AdviceModel(R.drawable.ic_coffee_cup,"Not to drink coffee and any other liquids from a cup that others drink from it"));
+        models.add(new AdviceModel(R.drawable.ic_covid_protection_mask_healthcare_contagion_icon,getActivity().getResources().getString(R.string.advice1)));
+        models.add(new AdviceModel(R.drawable.ic_water_wash_hand_washing_healthcare_icon,getActivity().getResources().getString(R.string.advice2)));
+        models.add(new AdviceModel(R.drawable.ic_avoid_public_places_icon,getActivity().getResources().getString(R.string.advice3)));
+        models.add(new AdviceModel(R.drawable.ic_raw_meat_no_avoid_eat_icon,getActivity().getResources().getString(R.string.advice4)));
+        models.add(new AdviceModel(R.drawable.ic_coffee_cup,getActivity().getResources().getString(R.string.advice5)));
 
-        models.add(new AdviceModel(R.drawable.ic_button_avoid_touching_face_stop_preventive_measure_coronavirus_icon,"Avoid contact with the eyes, nose and mouth with unwashed hands"));
-        models.add(new AdviceModel(R.drawable.ic_cleaning_door_knob_object_hygiene_icon,"Cleaning surfaces continuously and sterilizing them to maintain general cleanliness"));
-        models.add(new AdviceModel(R.drawable.ic_cough_tissue_close_mouth_icon,"Cover the mouth when coughing and sneezing with a tissue or bending the elbow and dispose of the tissue in the trash and wash hands immediately"));
-        models.add(new AdviceModel(R.drawable.ic_avoid_animal_dog_touching_virus_outbreak_icon,"Avoid contact with animals, whether alive or dead, and avoid contact with the brightest near these animals"));
-        models.add(new AdviceModel(R.drawable.ic_summer_cold_water_drink_ice_cube_hydrate_icon,"Maintaining that the throat is moist by drinking more water constantly"));
-        models.add(new AdviceModel(R.drawable.ic_nosmoking,"Smoking is harmful to your health, and hookah is a focus of viruses that you reap, especially in public places"));
-        models.add(new AdviceModel(R.drawable.ic_handshake,"Refrain from kissing and just shake hands"));
-        models.add(new AdviceModel(R.drawable.ic_sneezing_spread_germs_infection_illness_coronavirus_icon,"Avoid spitting on the floor "));
-        models.add(new AdviceModel(R.drawable.ic_high_temperature_avatar_hot_fever_coronavirus_icon,"Stay at home when you have any symptoms such as high temperature, coughing or sneezing "));
-        models.add(new AdviceModel(R.drawable.ic_thermometer_gun,"You must measure the temperature if you can be in crowded places "));
-
-
-
-
-
+        models.add(new AdviceModel(R.drawable.ic_button_avoid_touching_face_stop_preventive_measure_coronavirus_icon,getActivity().getResources().getString(R.string.advice6)));
+        models.add(new AdviceModel(R.drawable.ic_cleaning_door_knob_object_hygiene_icon,getActivity().getResources().getString(R.string.advice7)));
+        models.add(new AdviceModel(R.drawable.ic_cough_tissue_close_mouth_icon,getActivity().getResources().getString(R.string.advice8)));
+        models.add(new AdviceModel(R.drawable.ic_avoid_animal_dog_touching_virus_outbreak_icon,getActivity().getResources().getString(R.string.advice9)));
+        models.add(new AdviceModel(R.drawable.ic_summer_cold_water_drink_ice_cube_hydrate_icon,getActivity().getResources().getString(R.string.advice10)));
+        models.add(new AdviceModel(R.drawable.ic_nosmoking,getActivity().getResources().getString(R.string.advice11)));
+        models.add(new AdviceModel(R.drawable.ic_handshake,getActivity().getResources().getString(R.string.advice12)));
+        models.add(new AdviceModel(R.drawable.ic_sneezing_spread_germs_infection_illness_coronavirus_icon,getActivity().getResources().getString(R.string.advice13)));
+        models.add(new AdviceModel(R.drawable.ic_high_temperature_avatar_hot_fever_coronavirus_icon,getActivity().getResources().getString(R.string.advice14)));
+        models.add(new AdviceModel(R.drawable.ic_thermometer_gun,getActivity().getResources().getString(R.string.advice15)));
 
 
 
--- app/src/main/java/com/example/covid_19tracker/fragments/LanguagesFragment.java
+++ app/src/main/java/com/example/covid_19tracker/fragments/LanguagesFragment.java
@@ -1,10 +1,14 @@
 package com.example.covid_19tracker.fragments;
 
+import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -13,11 +17,15 @@ import androidx.navigation.Navigation;
 
 import com.example.covid_19tracker.R;
 
+import java.util.Locale;
+
 
 public class LanguagesFragment extends Fragment {
 
 
     ImageView backLanguageIv;
+    TextView tv_ar,tv_En;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,6 +38,8 @@ public class LanguagesFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_languages, container, false);
         backLanguageIv = view.findViewById(R.id.back_language_iv);
+        tv_ar=view.findViewById(R.id.arabic_tv);
+        tv_En=view.findViewById(R.id.english_tv);
         return view ;
     }
 
@@ -42,5 +52,26 @@ public class LanguagesFragment extends Fragment {
                 Navigation.findNavController(v).navigate(R.id.action_languagesFragment_to_settingsFragment);
             }
         });
+        tv_ar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setLocale("ar",v);
+            }
+        });
+        tv_En.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setLocale("en",v);
+            }
+        });
+    }
+    public void setLocale(String lang,View v){
+        Locale myLocale = new Locale(lang);
+        DisplayMetrics dm = getActivity().getResources().getDisplayMetrics();
+        Configuration conf = getActivity().getResources().getConfiguration();
+        conf.locale = myLocale;
+        getActivity().getResources().updateConfiguration(conf,dm);
+        Navigation.findNavController(v).navigate(R.id.languagesFragment);
+
     }
 }
